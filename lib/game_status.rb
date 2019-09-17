992cf9b1649d29ac124184345e98f32228a16170
Jesse Wenger <wengerj@gmail.com>
2019-09-17 02:00:44 +0000

    Add identification of winning combination

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,17 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+  ]
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def won?(board)
+
+  WIN_COMBINATIONS.detect do |combination|
+    if position_taken?(board, combination[0]) && position_taken?(board, combination[1]) && position_taken?(board, combination[2])
+      board[combination[0]] == board[combination[1]] && board[combination[0]] == board[combination[2]]
+    end
+  end
+end
